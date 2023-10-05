38756f1ca4f704d278c85e83cd03009734df9dc3
vladi <vladisla.sv.299563@gmail.com>
2023-10-05 11:08:50 +0600

    add homework

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_day date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,57 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+
+
+conn = psycopg2.connect(host="localhost",
+                        database="north",
+                        user="postgres",
+                        password="ktnj2023")
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/customers_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                        row
+                    )
+
+finally:
+    conn.close()
+
+
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/employees_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_day, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+                        row
+                    )
+finally:
+    conn.close()
+
+
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/orders_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+                        "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                        row
+                    )
+finally:
+    conn.close()
